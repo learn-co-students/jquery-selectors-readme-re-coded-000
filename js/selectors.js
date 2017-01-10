c7b5632c00f1c47a52dda20250c686a8bb0e295c
ALI ALRAWI <ali.developer90@gmail.com>
2017-01-10 16:13:52 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,23 @@
 'use-strict';
 
 // declare your functions here...
+function paragraphSelector () {
+  var p_tag=$('p');//document.getElementByTagName("p");
+  return p_tag;
+}
+function lastImageSelector() {
+  var img_tag=$('img:last');
+  return img_tag;
+}
+function ninjaBabySelector () {
+  var img_=$('#baby-ninja');
+  return img_;
+}
+function divSelector () {
+  var divs=$('.pics');
+  return divs;
+}
+function firstListItem () {
+  var li=  $('.pics ul li:first-child') ;
+  return li;
+}
